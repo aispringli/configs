f6bda270aef1fa882184af9125c40732f9a7183a
spring <spring@li>
2020-11-01 21:22:57 +0800

    k8s

--- k8s/generate_copy_cert.sh
+++ k8s/generate_copy_cert.sh
@@ -3,9 +3,9 @@ PORT=2222
 CONTROL_PLANE_IPS="server01"
 for host in ${CONTROL_PLANE_IPS}; do
     ssh ${USER}@$host -p ${PORT} "mkdir ­p /etc/kubernetes/pki/etcd"
-    scp -P ${PORT} -­r /etc/kubernetes/pki/ca.* ${USER}@$host:/etc/kubernetes/pki/
-    scp -P ${PORT} -­r /etc/kubernetes/pki/sa.* ${USER}@$host:/etc/kubernetes/pki/
-    scp -P ${PORT} ­-r /etc/kubernetes/pki/front­proxy­ca.* ${USER}@$host:/etc/kubernetes/pki/
-    scp -P ${PORT} ­-r /etc/kubernetes/pki/etcd/ca.* ${USER}@$host:/etc/kubernetes/pki/etcd/
-    scp -P ${PORT} ­-r /etc/kubernetes/admin.conf ${USER}@$host:/etc/kubernetes/
+    scp -P ${PORT} r /etc/kubernetes/pki/ca.* ${USER}@$host:/etc/kubernetes/pki/
+    scp -P ${PORT} r /etc/kubernetes/pki/sa.* ${USER}@$host:/etc/kubernetes/pki/
+    scp -P ${PORT} r /etc/kubernetes/pki/front­proxy­ca.* ${USER}@$host:/etc/kubernetes/pki/
+    scp -P ${PORT} r /etc/kubernetes/pki/etcd/ca.* ${USER}@$host:/etc/kubernetes/pki/etcd/
+    scp -P ${PORT} r /etc/kubernetes/admin.conf ${USER}@$host:/etc/kubernetes/
 done
